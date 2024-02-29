3551ce98d40dbbc2bfe7f157501c1a448defb74d
Prabs <prabash.wijethunga@igp.uu.se>
2024-02-29 17:28:25 +0100

    Readme and metacode altered
    I've made it a bit more clear now.

--- rotor.py
+++ rotor.py
@@ -13,6 +13,10 @@ class Rotor:
         alphabet has moved to a certain point (tnotch) it will signal the consequetive rotor( and its alphabet )
         to move one step forward."""
 
+        """NOTE! I will first introduce the new, scrambled alphabet, and make sure that the encryption and decr
+         yption can move from the plugboard to the reflector. THEN I will try to implement the actual rotation 
+          mechanism.. Somehow. """ 
+
         self.tnotch = tnotch #This is the turnover notch, which represents the letter at which the rotor will
                             # Cause the other rotor to turn one step.
         self.stand_alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
