a85c7ca8c026b5770712f33f4a51c83ea9709ed9
Prabs <prabash.wijethunga@igp.uu.se>
2024-02-29 17:07:21 +0100

    Update main.py
    Main.py is currently a testing ground

--- main.py
+++ main.py
@@ -11,4 +11,8 @@ A = k.encrypt("A")
 p = Plugboard("A-D", "D-C", "Z-E")
 print(p.encrypt(A))
 p.print_encrypted()
-#So far so good
+#Encryption, so far so good
+
+c= p.decrypt(3)
+print(k.decrypt(c))
+#Decryption, so far so good.
